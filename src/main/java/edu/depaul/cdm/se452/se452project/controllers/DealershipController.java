aef438908bddafc279ba682d00879ec22943a371
Darrin Kachadorian <dkachado@depaul.edu>
2022-05-20 20:44:21 -0400

    updated dealership to create a table

--- src/main/java/edu/depaul/cdm/se452/se452project/controllers/DealershipController.java
+++ src/main/java/edu/depaul/cdm/se452/se452project/controllers/DealershipController.java
@@ -9,16 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import edu.depaul.cdm.se452.se452project.entities.Dealership;
 import edu.depaul.cdm.se452.se452project.repositories.DealershipRepository;
-import edu.depaul.cdm.se452.se452project.services.DealershipService;
 
 @Controller
 public class DealershipController {
-
-    
+  
     DealershipRepository dealershipRepository;
-    DealershipService dealershipService;
  
-
     @Autowired
     public DealershipController(DealershipRepository dealershipRepository) {
         this.dealershipRepository = dealershipRepository;
